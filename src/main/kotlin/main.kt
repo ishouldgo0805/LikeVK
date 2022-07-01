aefed9656cafebd7cd716f254fcbe1e10716c57e
Dmitry Vinogradov <dmitrvino@gmail.com>
2022-07-01 14:20:45 +0300

    Changed

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,20 +1,12 @@
+var likes = 11
 fun main() {
-    var likes = 60
-    if (likes == 0) {
-        println("Тут пока ещё нет лайков")
-    } else if (likes == 1) {
-        println("Понравилось $likes человеку")
-    } else if (likes == 21) {
-        println("Понравилось $likes человеку")
-    } else if (likes == 31) {
-        println("Понравилось $likes человеку")
-    } else if (likes == 41) {
-        println("Понравилось $likes человеку")
-    } else if (likes == 51) {
-        println("Понравилось $likes человеку")
-    } else if (likes >= 2 && likes <= 20) {
-        println("Понравилось $likes людям")
-    } else if (likes >= 22 && likes <= 60) {
-        println("Понравилось $likes людям")
+
+    var solution = if (likes % 10 != 1) {
+        "Людям"
+    } else if (likes % 100 != 11) {
+        "Человеку"
+    } else {
+        "Людям"
     }
+    println("Понравилось $likes $solution")
 }
